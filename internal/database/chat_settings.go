9abbdebe7a5deca2dca441650dc7e4d8b32cd916
batmanm894-bit <batmanm894@gmail.com>
2026-07-16 21:41:52 +0530

    Update chat_settings.go
    Signed-off-by: batmanm894-bit <batmanm894@gmail.com>

--- internal/database/chat_settings.go
+++ internal/database/chat_settings.go
@@ -37,7 +37,6 @@ type ChatSettings struct {
 	Language              string     `bson:"language"`
 	RTMP                  RTMPConfig `bson:"rtmp_config"`
 	AssistantIndex        int        `bson:"ass_index,omitempty"`
-	ThumbnailsDisabled    bool       `bson:"no_thumb"`
 	PlayModeAdminsOnly    bool       `bson:"play_mode"`
 	CommandDelete         bool       `bson:"cmd_delete"`
 	CleanMode             bool       `bson:"clean_mode"`
